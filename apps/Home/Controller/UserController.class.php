bade903bd23cfecc1118491ed6cac50d54a7d9d7
CL.Lam <lamciuloeng@gmail.com>
2014-06-24 17:57:22 +0800

    Add the ThinkPHP framework

---
+++ apps/Home/Controller/UserController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+/**
+ * 
+ */
+class UserController extends Controller {
+	
+	function index() {
+		echo "hello,User!";
+	}
+}
---
+++ apps/Runtime/Cache/Home/3a3e284780de787c780ce06826d239a3.php
@@ -0,0 +1,81 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+	<header>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<title>MyShop V0.1</title>
+		
+		<!-- Bootstrap -->
+    	<link href="/Public/css/bootstrap.min.css" rel="stylesheet">
+    	<link href="/Public/css/bootstrap-theme.min.css" rel="stylesheet">
+    	
+    	
+    	
+    	<script src="/Public/js/jquery-1.11.1.min.js"></script>
+    	<script src="/Public/js/bootstrap.min.js"></script>
+    	
+    	
+	</header>
+	<body>
+		<!-- Fixed navbar -->
+	    <div class="navbar navbar-inverse navbar-fixed-top " role="navigation">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="/index.php">Changuan Hardware Factory</a>
+	        </div>
+	        <div class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav">
+	            <li class="active"><a href="#">首页</a></li>
+	            <li><a href="#about">公司简介</a></li>
+	            <li><a href="#about">公司风采</a></li>
+	            <li class="dropdown">
+	              <a href="#" class="dropdown-toggle" data-toggle="dropdown">产品展示 <b class="caret"></b></a>
+	              <ul class="dropdown-menu">
+	                <li><a href="#">地弹簧</a></li>
+	                <li><a href="#">闭门器</a></li>
+	                <li><a href="#">门夹</a></li>
+	                <li><a href="#">配件</a></li>
+	                <li><a href="#">门锁</a></li>
+	                <li><a href="#">浴室夹</a></li>
+	                <li><a href="#">玻璃门拉手</a></li>
+	                <li><a href="#">门执手</a></li>
+	                <li><a href="#">玻璃连接件</a></li>
+	                <li><a href="#">浴室固定件</a></li>
+	              </ul>
+	            </li>
+	            <li><a href="#contact">联系我们</a></li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </div>
+		
+			<div class="container">
+		<div class="jumbotron">
+				
+		  <h1>Hello, world!</h1>
+		  <p>...</p>
+		  <p><a class="btn btn-primary btn-lg" role="button">Learn more</a></p>
+		</div>
+			</div>
+		
+		
+	<div class="container">
+		<h1>Content</h1>
+	</div>
+
+		
+		
+			<div class="container">
+				<p>Footer</p>
+			</div>
+		
+	</body>
+</html>
